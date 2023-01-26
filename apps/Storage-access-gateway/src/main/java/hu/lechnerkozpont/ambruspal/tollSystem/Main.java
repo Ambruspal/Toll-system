bf512de78d0d0467320d0b65c9de7d528b1ff7f5
Ambruspal <phaal83@gmail.com>
2023-01-26 17:21:24 +0100

    Second commit

--- apps/Storage-access-gateway/src/main/java/hu/lechnerkozpont/ambruspal/tollSystem/Main.java
+++ apps/Storage-access-gateway/src/main/java/hu/lechnerkozpont/ambruspal/tollSystem/Main.java
@@ -2,6 +2,6 @@ package hu.lechnerkozpont.ambruspal.tollSystem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Toll-system!");
+        System.out.println("Hello world!");
     }
 }
